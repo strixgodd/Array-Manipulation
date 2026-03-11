ce7f989fe427a35faa4893e2c1cccffece71df04
Soumojit Baidya <soumojitbaidya80@gmail.com>
2026-03-11 18:59:20 +0530

    Modified by adding edge cases,R not done

--- powerful_integers.cpp
+++ powerful_integers.cpp
@@ -1,26 +1,51 @@
-class Solution {//brute force using hash map.
+class Solution {
 public:
     vector<int> powerfulIntegers(int x, int y, int bound) {//brute force using hash map
-        int sum=0;
+        vector<int> ans;
+        
         unordered_map<int,int> hash;
-
-        int i=0;
-        while(sum<=bound){
-            int j=0;
+        //edge case
+        if(x==1 && y==1){
+            if(2<=bound)
+            ans.push_back(2);
+            return ans;
+        }
+        int sum=0,i=0;
+        if(x==1){
             while(sum<=bound){
-                sum=pow(x,i)+pow(y,j);
+                sum=1+pow(y,i);
                 if(sum<=bound && hash.find(sum)==hash.end())
                     hash.insert({sum,1});
-                j++;
-                // cout<<sum<<" "<<i<<" "<<j<<" "<<endl;
-
+                i++;
             }
-            i++;
-            sum=pow(x,i)+pow(x,0);
         }
-        vector<int> ans;
+        else if(y==1){
+            while(sum<=bound){
+                sum=1+pow(x,i);
+                if(sum<=bound && hash.find(sum)==hash.end())
+                    hash.insert({sum,1});
+                i++;
+            }
+        }
+        //above are edge cases
+        else{
+            while(sum<=bound){
+                int j=0;
+                while(sum<=bound){       
+                    sum=pow(x,i)+pow(y,j);
+                    if(sum<=bound && hash.find(sum)==hash.end())
+                        hash.insert({sum,1});
+                    j++;
+                    // cout<<sum<<" "<<i<<" "<<j<<" "<<endl;
+
+                }
+                i++;
+                sum=pow(x,i)+pow(x,0);
+            }
+
+        }
         for(auto i:hash){
-            cout<<i.first<<" ";
+            // cout<<i.first<<" ";
             ans.push_back(i.first);
         }
         return ans;
